e7ad68179dfc649caa825acb483ab4ff988219d4
vidyasriraj <vidyasrirajkilli@gmail.com>
2024-09-18 18:11:10 +0530

    benchmark

--- buddy/benchmark_my_malloc.cpp
+++ buddy/benchmark_my_malloc.cpp
@@ -1,15 +1,52 @@
 #include "src/allocator.h"
 #include <benchmark/benchmark.h>
+#include <cstdlib>  // For malloc and free
 
-static void BM_CustomMalloc(benchmark::State& state) {
-    Allocator allocator;
+Allocator allocator;
+
+static void BM_SystemMalloc(benchmark::State& state) {
     for (auto _ : state) {
-        void* ptr = allocator.my_malloc(state.range(0));
-        allocator.my_free(ptr);
+        // Perform a single allocation and deallocation cycle
+        void* ptr = malloc(256);
+        if (ptr) {
+            free(ptr);
+        }
+        
+        ptr = malloc(8);
+        if (ptr) {
+            free(ptr);
+        }
+        
+        ptr = malloc(128);
+        if (ptr) {
+            free(ptr);
+        }
     }
 }
-BENCHMARK(BM_CustomMalloc)->Range(8, 8 << 10);
+BENCHMARK(BM_SystemMalloc);
+
+static void BM_CustomMalloc(benchmark::State& state) {
+    for (auto _ : state) {
+        // Perform a single allocation and deallocation cycle
+        void* ptr = allocator.my_malloc(256);
+        if (ptr) {
+            allocator.my_free(ptr);
+        }
+        
+        ptr = allocator.my_malloc(8);
+        
+        if (ptr) {
+            allocator.my_free(ptr);
+        }
+        
+        ptr = allocator.my_malloc(128);
+        if (ptr) {
+            allocator.my_free(ptr);
+        }
+    }
+}
+BENCHMARK(BM_CustomMalloc);
+
 
 
 BENCHMARK_MAIN();
-
