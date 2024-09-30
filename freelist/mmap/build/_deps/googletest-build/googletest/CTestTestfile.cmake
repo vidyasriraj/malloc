5cc3d5d0093896b83b9a41d64e84d2b932f8bf5a
vidyasriraj <vidyasrirajkilli@gmail.com>
2024-09-30 12:52:50 +0530

    src

--- freelist/mmap/build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/killi-pt7716/Desktop/malloc/freelist/mmap/build/_deps/googletest-src/googletest
-# Build directory: /home/killi-pt7716/Desktop/malloc/freelist/mmap/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- freelist/sbrk/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/killi-pt7716/Desktop/malloc/freelist/sbrk
-# Build directory: /home/killi-pt7716/Desktop/malloc/freelist/sbrk/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(AllTests "testing")
-set_tests_properties(AllTests PROPERTIES  _BACKTRACE_TRIPLES "/home/killi-pt7716/Desktop/malloc/freelist/sbrk/CMakeLists.txt;43;add_test;/home/killi-pt7716/Desktop/malloc/freelist/sbrk/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
